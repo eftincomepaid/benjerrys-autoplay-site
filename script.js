c34eccf1caebfb230efa3562911217e8cc03b3ab
EftAtoPaidauuu <bhomevacation@gmail.com>
2025-09-03 01:49:25 -0700

    Removed unused slideshow script

--- script.js
+++
@@ -1,7 +0,0 @@
-const slides = document.querySelectorAll('.slide');
-let i = 0;
-setInterval(() => {
-  slides[i].classList.remove('active');
-  i = (i + 1) % slides.length;
-  slides[i].classList.add('active');
-}, 3500);
